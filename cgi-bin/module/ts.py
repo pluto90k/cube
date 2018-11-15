ad08c76fd5333019a4fd2802228c09016bfd77ee
pluto90k <pluto90k@gmail.com>
2018-11-16 02:05:52 +0900

    STTS sample count and duration parse

--- cgi-bin/module/ts.py
+++ cgi-bin/module/ts.py
@@ -9,9 +9,28 @@ class TS(MP4):
 		super(TS,self).__init__(file_name)
 		self.fileName = file_name
 
+	def _parse(self, stts, offset, duration, timescale):
+
+		num = time = t_temp = n_temp = 0
+		min = offset * timescale
+		max = (offset + duration) * timescale
+
+		for data in stts:
+			for n in range(data['count']):
+				n_temp += 1
+				t_temp += data['delta']
+				if t_temp > max: return (num, time)
+
+				num = n_temp
+
+				if t_temp > min:
+					time += data['delta']
+
 	def segment(self, seq, duration):
 		atom = self.dic()['atom']
 		traks = atom['moov']['trak']
+
+		timeoffset = seq * duration
 
 		for trak in traks:
 			if trak['mdia']['hdlr']['handler-type'] == 'vide':
@@ -21,8 +40,9 @@ class TS(MP4):
 				stsz = stbl['stsz']['entry']	#Sample Size
 				stsc = stbl['stsc']['entry']	#Sample Count Per Chunk
 				stss = stbl['stss']['entry']
-				stts = stbl['stts']['entry']
-				print stss
+				stts = stbl['stts']['entry']	#Sample Count PlayTime
+				print (mdhd)
+				print self._parse(stts, timeoffset, duration, mdhd['timescale'])
 
 			elif trak['mdia']['hdlr']['handler-type'] == 'soun':
 				mdhd = trak['mdia']['mdhd']
@@ -31,7 +51,8 @@ class TS(MP4):
 				stsz = stbl['stsz']['entry']	#Sample Size
 				stsc = stbl['stsc']['entry']	#Sample Count Per Chunk
 				stts = stbl['stts']['entry']	#Sample Count PlayTime
-				print stts
+				print (mdhd)
+				print self._parse(stts, timeoffset, duration, mdhd['timescale'])
 
 		return self
 
@@ -40,4 +61,4 @@ class TS(MP4):
 
 
 if __name__ == '__main__':
-	TS('../../BigBuckBunny.mp4').segment(0, 10)
+	TS('../../BigBuckBunny.mp4').segment(3, 10)
