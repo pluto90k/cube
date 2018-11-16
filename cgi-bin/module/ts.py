0a5e616e48a243d5e4623f232a038932a75e44d4
pluto90k <pluto90k@gmail.com>
2018-11-17 02:34:25 +0900

    File Offet And Size Per sample

--- cgi-bin/module/ts.py
+++ cgi-bin/module/ts.py
@@ -9,10 +9,36 @@ class TS(MP4):
 		super(TS,self).__init__(file_name)
 		self.fileName = file_name
 
-	def _parse(self, stts, offset, duration, timescale):
+	def _getKey(self, item):
+		return item['pts']
 
-		num = time = t_temp = 0
-		offset_num = 1
+	def _findChunkIndex(self, stsc, num):
+		count = 0
+		for obj in stsc:
+			count += obj['sample_per_chunk']
+			if num < count:
+				return obj['first_chunk_index']
+
+		return 0
+	def _sample(self, trak, offset, duration):
+		sample = []
+		type = trak['mdia']['hdlr']['handler-type']
+
+		if type != 'vide' and type != 'soun':
+			return
+
+		mdhd = trak['mdia']['mdhd']
+		stbl = trak['mdia']['minf']['stbl']
+		stco = stbl['stco']['entry']	#Chunk Offset
+		stsz = stbl['stsz']['entry']	#Sample Size
+		stsc = stbl['stsc']['entry']	#Sample Count Per Chunk
+		stts = stbl['stts']['entry']	#Sample Count PlayTime
+		#stss = stbl['stss']['entry']
+		#print stco
+
+		timescale = mdhd['timescale']
+		time = t_temp = 0
+		num = 1
 
 		min = offset * timescale
 		max = (offset + duration) * timescale
@@ -20,47 +46,49 @@ class TS(MP4):
 		for data in stts:
 			for n in range(data['count']):
 				t_temp += data['delta']
-				if t_temp > max: return (offset_num, num, time)
+				if t_temp > max: break
 				if t_temp > min:
+					pack = {
+						"id": num,
+						"type": str(type),
+						"pts": time / float(timescale),
+						"chunk_offset": stco[self._findChunkIndex(stsc, num)]['chunk_offset'],
+						"sample_size": stsz[num - 1]['entry_size']
+					}
+
+					if type == 'vide':
+						pack['dts'] = t_temp / float(timescale)
+
+					sample.append(pack)
 					time += data['delta']
-				else:
-					offset_num += 1
 
 				num += 1
+
+		return sample
 
 	def segment(self, seq, duration):
 		atom = self.dic()['atom']
 		traks = atom['moov']['trak']
 
 		timeoffset = seq * duration
+		sample = []
 
 		for trak in traks:
-			if trak['mdia']['hdlr']['handler-type'] == 'vide':
-				mdhd = trak['mdia']['mdhd']
-				stbl = trak['mdia']['minf']['stbl']
-				stco = stbl['stco']['entry']	#Chunk Offset
-				stsz = stbl['stsz']['entry']	#Sample Size
-				stsc = stbl['stsc']['entry']	#Sample Count Per Chunk
-				stss = stbl['stss']['entry']
-				stts = stbl['stts']['entry']	#Sample Count PlayTime
+			out = self._sample(trak, timeoffset, duration)
+			if out: sample.extend(out)
 
-				print self._parse(stts, timeoffset, duration, mdhd['timescale'])
+		sample = sorted(sample, key=self._getKey)
+		print sample
 
-			elif trak['mdia']['hdlr']['handler-type'] == 'soun':
-				mdhd = trak['mdia']['mdhd']
-				stbl = trak['mdia']['minf']['stbl']
-				stco = stbl['stco']['entry']	#Chunk Offset
-				stsz = stbl['stsz']['entry']	#Sample Size
-				stsc = stbl['stsc']['entry']	#Sample Count Per Chunk
-				stts = stbl['stts']['entry']	#Sample Count PlayTime
 
-				print self._parse(stts, timeoffset, duration, mdhd['timescale'])
+
+
+
 
 		return self
 
 	def buffer(self):
 		print 'buffer'
 
-
 if __name__ == '__main__':
-	TS('../../BigBuckBunny.mp4').segment(2, 10)
+	TS('../../BigBuckBunny.mp4').segment(0, 10)
